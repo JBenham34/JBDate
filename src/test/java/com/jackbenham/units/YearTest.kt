485558f90a72ad58d7c5a0717352d3b1d3142868
Jack <jack.benham34@gmail.com>
2024-06-09 02:46:17 +0100

    YearTest to 100% coverage

--- src/test/java/com/jackbenham/units/YearTest.kt
+++ src/test/java/com/jackbenham/units/YearTest.kt
@@ -1,5 +1,7 @@
 package com.jackbenham.units
 
+import com.jackbenham.relations.MonthInYear
+import com.jackbenham.relations.QuarterInYear
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
@@ -15,12 +17,19 @@ class YearTest {
     }
 
     @Test
-    fun testGetYYYY() {
+    fun testFromKey() {
+        val y2020 = Year.fromKey(2020)
+        val y2020Again = Year.fromKey(2020)
+        assertSame(y2020, y2020Again)
+
+        val y2021 = Year.fromKey(2021)
+        assertNotSame(y2021, y2020)
+    }
+
+    @Test
+    fun testGetYs() {
+        assertEquals(20, Year(2020).getYY())
         assertEquals(2020, Year(2020).getYYYY())
-        assertEquals(1444, Year(1444).getYYYY())
-        assertEquals(1821, Year(1821).getYYYY())
-        assertEquals(1948, Year(1948).getYYYY())
-        assertEquals(-2150, Year(-2150).getYYYY())
     }
 
     @Test
@@ -57,5 +66,39 @@ class YearTest {
         assertEquals(Year(2020), Year(2020))
         assertNotEquals(Year(2020), Year(2021))
         assertNotEquals(Year(2020), Year(2021).APR)
+    }
+
+    @Test
+    fun testMonthPseudoEnums() {
+        assertEquals(Month(Year(2020), MonthInYear.JAN), Year(2020).JAN)
+        assertEquals(Month(Year(2020), MonthInYear.FEB), Year(2020).FEB)
+        assertEquals(Month(Year(2020), MonthInYear.MAR), Year(2020).MAR)
+        assertEquals(Month(Year(2020), MonthInYear.APR), Year(2020).APR)
+        assertEquals(Month(Year(2020), MonthInYear.MAY), Year(2020).MAY)
+        assertEquals(Month(Year(2020), MonthInYear.JUN), Year(2020).JUN)
+        assertEquals(Month(Year(2020), MonthInYear.JUL), Year(2020).JUL)
+        assertEquals(Month(Year(2020), MonthInYear.AUG), Year(2020).AUG)
+        assertEquals(Month(Year(2020), MonthInYear.SEP), Year(2020).SEP)
+        assertEquals(Month(Year(2020), MonthInYear.OCT), Year(2020).OCT)
+        assertEquals(Month(Year(2020), MonthInYear.NOV), Year(2020).NOV)
+        assertEquals(Month(Year(2020), MonthInYear.DEC), Year(2020).DEC)
+    }
+
+    @Test
+    fun testQuarterPseudoEnums() {
+        assertEquals(Quarter(Year(2020), QuarterInYear.Q1), Year(2020).Q1)
+        assertEquals(Quarter(Year(2020), QuarterInYear.Q2), Year(2020).Q2)
+        assertEquals(Quarter(Year(2020), QuarterInYear.Q3), Year(2020).Q3)
+        assertEquals(Quarter(Year(2020), QuarterInYear.Q4), Year(2020).Q4)
+    }
+
+    @Test
+    fun testString() {
+        assertEquals("2020", Year(2020).toString())
+    }
+
+    @Test
+    fun testHashCode() {
+        assertEquals(2020, Year(2020).hashCode())
     }
 }
