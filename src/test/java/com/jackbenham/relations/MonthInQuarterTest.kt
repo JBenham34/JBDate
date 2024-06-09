41adaa61630141ddc6524778c334d8a4f0af877c
Jack <jack.benham34@gmail.com>
2024-06-09 01:06:33 +0100

    Improve MonthInQuarterTest

--- src/test/java/com/jackbenham/relations/MonthInQuarterTest.kt
+++ src/test/java/com/jackbenham/relations/MonthInQuarterTest.kt
@@ -5,21 +5,30 @@ import org.junit.jupiter.api.Test
 
 internal class MonthInQuarterTest {
     @Test
-    fun next() {
+    fun testFromKey() {
+        assertNull(MonthInQuarter.fromKey(-1))
+        assertSame(MonthInQuarter.FIRST, MonthInQuarter.fromKey(0))
+        assertSame(MonthInQuarter.MIDDLE, MonthInQuarter.fromKey(1))
+        assertSame(MonthInQuarter.LAST, MonthInQuarter.fromKey(2))
+        assertNull(MonthInQuarter.fromKey(3))
+    }
+
+    @Test
+    fun testNext() {
         assertSame(MonthInQuarter.MIDDLE, MonthInQuarter.FIRST.next())
         assertSame(MonthInQuarter.LAST, MonthInQuarter.MIDDLE.next())
         assertSame(MonthInQuarter.FIRST, MonthInQuarter.LAST.next())
     }
 
     @Test
-    fun prev() {
+    fun testPrev() {
         assertSame(MonthInQuarter.LAST, MonthInQuarter.FIRST.prev())
         assertSame(MonthInQuarter.FIRST, MonthInQuarter.MIDDLE.prev())
         assertSame(MonthInQuarter.MIDDLE, MonthInQuarter.LAST.prev())
     }
 
     @Test
-    fun add() {
+    fun testAdd() {
         assertSame(MonthInQuarter.MIDDLE, MonthInQuarter.FIRST.add(1))
         assertSame(MonthInQuarter.FIRST, MonthInQuarter.FIRST.add(12))
         assertSame(MonthInQuarter.FIRST, MonthInQuarter.FIRST.add(24))
@@ -28,5 +37,21 @@ internal class MonthInQuarterTest {
         assertSame(MonthInQuarter.MIDDLE, MonthInQuarter.LAST.add(-1))
         assertSame(MonthInQuarter.LAST, MonthInQuarter.LAST.add(-3))
         assertSame(MonthInQuarter.LAST, MonthInQuarter.LAST.add(-12))
+    }
+
+    @Test
+    fun testGetMonthInYear() {
+        assertSame(MonthInYear.JAN, MonthInQuarter.FIRST.getMonthInYear(QuarterInYear.Q1))
+        assertSame(MonthInYear.FEB, MonthInQuarter.MIDDLE.getMonthInYear(QuarterInYear.Q1))
+        assertSame(MonthInYear.MAR, MonthInQuarter.LAST.getMonthInYear(QuarterInYear.Q1))
+        assertSame(MonthInYear.APR, MonthInQuarter.FIRST.getMonthInYear(QuarterInYear.Q2))
+        assertSame(MonthInYear.MAY, MonthInQuarter.MIDDLE.getMonthInYear(QuarterInYear.Q2))
+        assertSame(MonthInYear.JUN, MonthInQuarter.LAST.getMonthInYear(QuarterInYear.Q2))
+        assertSame(MonthInYear.JUL, MonthInQuarter.FIRST.getMonthInYear(QuarterInYear.Q3))
+        assertSame(MonthInYear.AUG, MonthInQuarter.MIDDLE.getMonthInYear(QuarterInYear.Q3))
+        assertSame(MonthInYear.SEP, MonthInQuarter.LAST.getMonthInYear(QuarterInYear.Q3))
+        assertSame(MonthInYear.OCT, MonthInQuarter.FIRST.getMonthInYear(QuarterInYear.Q4))
+        assertSame(MonthInYear.NOV, MonthInQuarter.MIDDLE.getMonthInYear(QuarterInYear.Q4))
+        assertSame(MonthInYear.DEC, MonthInQuarter.LAST.getMonthInYear(QuarterInYear.Q4))
     }
 }
