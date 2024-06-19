3b8b1af7626ce62317bb73c9b442ae59000b1ad2
Jack <jack.benham34@gmail.com>
2024-06-19 20:15:53 +0100

    Add .FIRST etc to Quarter to get Months

--- src/test/java/com/jackbenham/units/QuarterTest.kt
+++ src/test/java/com/jackbenham/units/QuarterTest.kt
@@ -1,6 +1,5 @@
 package com.jackbenham.units
 
-import com.jackbenham.relations.MonthInYear
 import com.jackbenham.relations.QuarterInYear
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
